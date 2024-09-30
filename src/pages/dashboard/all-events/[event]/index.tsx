fecec83a73249b5fcb28b18f7fb0f122c67821df
Ary0405 <aryansethia2003@gmail.com>
2024-09-30 23:51:24 +0530

    valo location changed

--- src/pages/dashboard/all-events/[event]/index.tsx
+++ src/pages/dashboard/all-events/[event]/index.tsx
@@ -11,9 +11,7 @@ import {
 import { useRouter } from "next/router";
 import { Layout } from "~/components/layout";
 import { api } from "~/utils/api";
-import {
-  FiShoppingCart,
-} from "react-icons/fi";
+import { FiShoppingCart } from "react-icons/fi";
 import { useSession } from "next-auth/react";
 
 const EventPage = () => {
@@ -152,7 +150,8 @@ const EventPage = () => {
         </Box>
       </Box> */}
 
-      <SimpleGrid templateColumns={{ base: '1fr', md: '67% 33%' }}
+      <SimpleGrid
+        templateColumns={{ base: "1fr", md: "67% 33%" }}
         maxW="6xl"
         mx="auto"
         borderRadius="30px"
@@ -162,13 +161,31 @@ const EventPage = () => {
         w="90%"
       >
         <Box>
-          <Flex justifyContent="space-between" alignItems="flex-start" mb={8} py={8} px={8}
+          <Flex
+            justifyContent="space-between"
+            alignItems="flex-start"
+            mb={8}
+            py={8}
+            px={8}
           >
             <Box>
-              <Text as="h1" fontSize="4xl" color="#F4AC18" mb={7} fontWeight="bold">
+              <Text
+                as="h1"
+                fontSize="4xl"
+                color="#F4AC18"
+                mb={7}
+                fontWeight="bold"
+              >
                 {data.name}
               </Text>
-              <Text as="h1" fontSize="2xl" color="#F4AC18" mb={4} fontStyle="italic" fontWeight="bold">
+              <Text
+                as="h1"
+                fontSize="2xl"
+                color="#F4AC18"
+                mb={4}
+                fontStyle="italic"
+                fontWeight="bold"
+              >
                 Rules and Guidelines
               </Text>
               <Text fontSize="xl" mb={6} color="gray.300">
@@ -178,70 +195,93 @@ const EventPage = () => {
           </Flex>
         </Box>
 
-        <Box borderLeft={{ base: '0px', md: "1px solid #F4AC18" }}
-          borderTop={{ base: '1px solid #F4AC18', md: "0px" }}
-          py={6} px={6} bg="#181818" borderTopRightRadius={{ base: '0px', md: "30px" }}
+        <Box
+          borderLeft={{ base: "0px", md: "1px solid #F4AC18" }}
+          borderTop={{ base: "1px solid #F4AC18", md: "0px" }}
+          py={6}
+          px={6}
+          bg="#181818"
+          borderTopRightRadius={{ base: "0px", md: "30px" }}
           borderBottomRightRadius="30px"
-          borderBottomLeftRadius={{ base: '30px', md: "0px" }}
+          borderBottomLeftRadius={{ base: "30px", md: "0px" }}
         >
           <Flex direction="column" gap={4} color="gray.100" mt={2}>
-
             <Flex direction="row" mb={1}>
-              <Box w="0" border="6px solid #C52C10" h="auto" borderRadius="50px"
-              >
-              </Box>
+              <Box
+                w="0"
+                border="6px solid #C52C10"
+                h="auto"
+                borderRadius="50px"
+              ></Box>
               <Flex direction="column" ml={6}>
-                <Text as="h1" fontSize="xl" mb={4} fontWeight="bold" color="gray.100">
+                <Text
+                  as="h1"
+                  fontSize="xl"
+                  mb={4}
+                  fontWeight="bold"
+                  color="gray.100"
+                >
                   DATE
                 </Text>
                 <Text fontSize="l" color="gray.100" mb={6}>
-                  {
-                    new Date(data.dateFrom).toLocaleDateString("en-US", {
-                      month: "short",
-                      day: "numeric",
-                    })
-                  }
+                  {new Date(data.dateFrom).toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                  })}
                   {" - "}
-                  {
-                    new Date(data.dateFrom).getMonth() === new Date(data.dateTo).getMonth()
-                      ? new Date(data.dateTo).toLocaleDateString("en-US", { day: "numeric" })
-                      : new Date(data.dateTo).toLocaleDateString("en-US", {
-                        month: "short",
+                  {new Date(data.dateFrom).getMonth() ===
+                  new Date(data.dateTo).getMonth()
+                    ? new Date(data.dateTo).toLocaleDateString("en-US", {
                         day: "numeric",
                       })
-                  }
+                    : new Date(data.dateTo).toLocaleDateString("en-US", {
+                        month: "short",
+                        day: "numeric",
+                      })}
                   {", "}
-                  {
-                    new Date(data.dateTo).getFullYear()
-                  }
+                  {new Date(data.dateTo).getFullYear()}
                 </Text>
                 <Text as="h1" fontSize="xl" mb={4} fontWeight="bold">
                   Location
                 </Text>
                 <Text fontSize="l" color="gray.100" mb={4}>
-                  Indoor Sports Complex
+                  {data.name === "Valorant"
+                    ? "Online"
+                    : "Indoor Sports Complex"}
                 </Text>
               </Flex>
             </Flex>
-            <Box w="95%" h="0" borderBottom="1px solid" borderColor="gray.100"
+            <Box
+              w="95%"
+              h="0"
+              borderBottom="1px solid"
+              borderColor="gray.100"
               alignSelf="center"
-            >
-            </Box>
+            ></Box>
 
             <Box w="100%">
-              <Flex direction="row" mb={0} mt={3} w="100%" >
-                <Flex direction="column" pl="36px" w="100%" >
+              <Flex direction="row" mb={0} mt={3} w="100%">
+                <Flex direction="column" pl="36px" w="100%">
                   <Flex direction="row" w="100%">
-
                     <Flex direction="column" w="50%">
-                      <Text as="h1" fontSize="lg" fontWeight="bold" color="gray.100">
+                      <Text
+                        as="h1"
+                        fontSize="lg"
+                        fontWeight="bold"
+                        color="gray.100"
+                      >
                         Price
                       </Text>
-                      <Text fontSize="xl" color="gray.100" mb={4} fontWeight="extrabold">
+                      <Text
+                        fontSize="xl"
+                        color="gray.100"
+                        mb={4}
+                        fontWeight="extrabold"
+                      >
                         ₹{data.pricePerPlayer}
                       </Text>
                     </Flex>
-                    {session &&
+                    {session && (
                       <Flex direction="column" w="50%">
                         <Button
                           onClick={() => router.push("/dashboard/cart")}
@@ -251,11 +291,12 @@ const EventPage = () => {
                           Cart
                         </Button>
                       </Flex>
-                    }
+                    )}
                   </Flex>
 
                   <Text fontSize="l" color="gray.100">
-                    Team Size: <b>{data.minPlayers}</b> - <b>{data.maxPlayers}</b>
+                    Team Size: <b>{data.minPlayers}</b> -{" "}
+                    <b>{data.maxPlayers}</b>
                   </Text>
                   <Text fontSize="l" color="gray.100">
                     Category: <b>{data.category}</b>
