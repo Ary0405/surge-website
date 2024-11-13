77241f8df1a5f4e02c0de976ddfd52b22721a61d
Rohit JG <jgrohit.exe@gmail.com>
2024-11-14 01:29:53 +0530

    changed carousel to latest 6 sport and fixed build error

--- src/pages/scoreboard/carousel/index.tsx
+++ src/pages/scoreboard/carousel/index.tsx
@@ -38,103 +38,111 @@ function Scoreboard() {
         boxShadow="2xl"
         backgroundSize="cover"
         fontFamily="poppins"
+        h="750px"  // Fixed height for the box
+        overflowY="auto"  // Allow vertical scrolling within the box
       >
-        <Box mb={3} key={activeSport}>
-          <Text fontSize="4xl" fontWeight="bold" mb={3} textAlign="center" color={"#F3AB17"}>
-            {activeSport}
-          </Text>
-          {Object.entries(matchData[activeSport]).map(([category, matches]) => (
-            <Box mb={3} key={category}>
-              <Text fontSize="3xl" fontWeight="bold" mb={3} textAlign="center">
-                {category}
-              </Text>
-              <VStack spacing={2}>
-                {matches.map((match, index) => (
-                  <Flex
-                    key={index}
-                    w="100%"
-                    p={2}
-                    borderRadius="md"
-                    justifyContent="space-between"
-                    align="center"
-                  >
+        {activeSport && matchData[activeSport] ? (
+          <Box mb={3} key={activeSport}>
+            <Text fontSize="4xl" fontWeight="bold" mb={3} textAlign="center" color={"#F3AB17"}>
+              {activeSport}
+            </Text>
+            {Object.entries(matchData[activeSport]).map(([category, matches]) => (
+              <Box mb={3} key={category}>
+                <Text fontSize="3xl" fontWeight="bold" mb={3} textAlign="center">
+                  {category}
+                </Text>
+                <VStack spacing={2}>
+                  {matches.slice(-6).map((match, index) => (
                     <Flex
-                      align="center"
-                      justify="flex-start"
-                      bg="orange.500"
-                      color="white"
-                      transform="skew(-20deg)"
-                      px={4}
-                      py={2}
+                      key={index}
+                      w="100%"
+                      p={2}
                       borderRadius="md"
-                      boxShadow="md"
-                      flex="1"
+                      justifyContent="space-between"
+                      align="center"
                     >
-                      <Text fontWeight="bold" fontSize="lg" textAlign="left" transform="skew(20deg)">
-                        {match.team1}
-                      </Text>
-                    </Flex>
-                    <Flex align="center" justify="center" flex="0.4">
-                      <Box
-                        fontSize="xl"
-                        fontWeight="bold"
-                        p={2}
-                        borderRadius="md"
+                      <Flex
+                        align="center"
+                        justify="flex-start"
+                        bg="orange.500"
+                        color="white"
                         transform="skew(-20deg)"
-                        mx={1}
-                        boxShadow="md"
-                        minW="40px"
-                        textAlign="center"
-                        ml={-3}
-                        color="black"
-                        bg={(match.win == 1) ? "#97eb0f" : ((match.win == 2) ? "#d93300" : "yellow.400")}
-                      >
-                        <Text transform="skew(20deg)">{match.score1}</Text>
-                      </Box>
-                      <Text fontSize="xl" fontWeight="bold" mx={1}>
-                        :
-                      </Text>
-                      <Box
-                        fontSize="xl"
-                        fontWeight="bold"
-                        p={2}
+                        px={4}
+                        py={2}
                         borderRadius="md"
-                        transform="skew(20deg)"
-                        mx={1}
                         boxShadow="md"
-                        minW="40px"
-                        textAlign="center"
-                        zIndex="1"
-                        color="black"
-                        bg={(match.win == 2) ? "#97eb0f" : ((match.win == 1) ? "#d93300" : "yellow.400")}
+                        flex="1"
                       >
-                        <Text transform="skew(-20deg)">{match.score2}</Text>
-                      </Box>
+                        <Text fontWeight="bold" fontSize="lg" textAlign="left" transform="skew(20deg)">
+                          {match.team1}
+                        </Text>
+                      </Flex>
+                      <Flex align="center" justify="center" flex="0.4">
+                        <Box
+                          fontSize="xl"
+                          fontWeight="bold"
+                          p={2}
+                          borderRadius="md"
+                          transform="skew(-20deg)"
+                          mx={1}
+                          boxShadow="md"
+                          minW="40px"
+                          textAlign="center"
+                          ml={-3}
+                          color="black"
+                          bg={(match.win == 1) ? "#97eb0f" : ((match.win == 2) ? "#d93300" : "yellow.400")}
+                        >
+                          <Text transform="skew(20deg)">{match.score1}</Text>
+                        </Box>
+                        <Text fontSize="xl" fontWeight="bold" mx={1}>
+                          :
+                        </Text>
+                        <Box
+                          fontSize="xl"
+                          fontWeight="bold"
+                          p={2}
+                          borderRadius="md"
+                          transform="skew(20deg)"
+                          mx={1}
+                          boxShadow="md"
+                          minW="40px"
+                          textAlign="center"
+                          zIndex="1"
+                          color="black"
+                          bg={(match.win == 2) ? "#97eb0f" : ((match.win == 1) ? "#d93300" : "yellow.400")}
+                        >
+                          <Text transform="skew(-20deg)">{match.score2}</Text>
+                        </Box>
+                      </Flex>
+                      <Flex
+                        align="center"
+                        justify="flex-end"
+                        bg="orange.500"
+                        color="white"
+                        transform="skew(20deg)"
+                        px={4}
+                        py={2}
+                        borderRadius="md"
+                        boxShadow="md"
+                        flex="1"
+                        ml={-3}
+                        zIndex="0"
+                      >
+                        <Text fontWeight="bold" fontSize="lg" textAlign="right" transform="skew(-20deg)">
+                          {match.team2}
+                        </Text>
+                      </Flex>
                     </Flex>
-                    <Flex
-                      align="center"
-                      justify="flex-end"
-                      bg="orange.500"
-                      color="white"
-                      transform="skew(20deg)"
-                      px={4}
-                      py={2}
-                      borderRadius="md"
-                      boxShadow="md"
-                      flex="1"
-                      ml={-3}
-                      zIndex="0"
-                    >
-                      <Text fontWeight="bold" fontSize="lg" textAlign="right" transform="skew(-20deg)">
-                        {match.team2}
-                      </Text>
-                    </Flex>
-                  </Flex>
-                ))}
-              </VStack>
-            </Box>
-          ))}
-        </Box>
+                  ))}
+                </VStack>
+              </Box>
+            ))}
+          </Box>
+        ) : (
+          <Text fontSize="2xl" textAlign="center" color="gray.300">
+            Loading...
+          </Text>
+        )}
       </Box>
     </Layout>
   );
